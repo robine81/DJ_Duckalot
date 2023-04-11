97c3ae36f602ddad92284708f1321fe02557b261
robine81 <robine.westberg@gmail.com>
2023-04-11 12:48:26 +0200

    A functioning game

--- src/js/main.js
+++ src/js/main.js
@@ -1,90 +1,232 @@
-let currentGame;
-let currentDJ;
+//draw the canvas
+let canvas = document.getElementById("my-canvas");
+let ctx = canvas.getContext("2d");
+canvas.style.border = "2px solid black";
 
-document.getElementById('game-board').style.display = 'none';
-const myCanvas = document.getElementById('the-canvas');
-const ctx = myCanvas.getContext('2d');
+//canvas variables
+let height = canvas.height - 200;
+let middle = canvas.width / 2 + 60;
 
-document.getElementById('game-over-text').style.display = 'none'
+//player car dimensions
+let playerWidth = 130;
+let playerHeight = 150;
+let playerX = 0;
+let playerY = canvas.height / 2 - playerHeight - 5;
 
-document.getElementById('start-button').onclick = () => {
-    startGame();
-}
+//all varibles
+let startBtn = document.querySelector("#start-button");
+let restartBtn = document.querySelector("#restart-button");
+let logo = document.querySelector("#logo-img");
+let gameLogo = document.querySelector("#game-logo");
+let arrows = document.querySelector("#arrows-img-div");
+let scoreDiv = document.querySelector("#score-div");
+let scoreElement = document.querySelector("#score");
+let gameBoard = document.querySelector("#game-board");
 
-document.onkeydown = (e) => {
-    let whereToGo = e.keyCode;
-    currentGame.dj.moveDJ(whereToGo);
-}
+//background
+let bg = new Image();
+bg.src = "/images/crowd.jpeg";
+let bg1Y = 0;
+let bg2Y = -canvas.height;
 
+//DJ and all obstracles
+let dj = new Image();
+dj.src = "/images/DJ.jpeg";
+let carPink = new Image();
+carPink.src = "/images/beer.png";
+let carWhite = new Image();
+carWhite.src = "/images/underwear.jpeg";
+let carYellow = new Image();
+carYellow.src = "/images/cd.jpeg";
+let bus = new Image();
+bus.src = "/images/rose.jpeg";
+// let motorcycle = new Image();
+// motorcycle.src = "../images/motorcycle.png";
+
+let moveRight = false;
+let moveLeft = false;
+let moveUp = false
+let moveDown = false
+
+//variables for sizes and movements
+let speed = 5;
+let intervalId = 0;
+let isGameOver = false;
+let score = 0;
+
+//generate random 'X' positions on the road for the traffic
+let randomXPlacement = () => {
+  let biggestX = canvas.width - 350;
+  let smallestX = 55;
+  let randomX = Math.floor(
+    Math.random() * (biggestX - smallestX + 1) + smallestX
+  );
+  console.log(randomX);
+  return randomX;
+};
+//traffic cars information
+let throwArray = [
+  { img: carPink, x: randomXPlacement(), y: -200, width: 110, height: 170 },
+  {
+    img: carWhite,
+    x: randomXPlacement() - 300,
+    y: -800,
+    width: 130,
+    height: 220,
+  },
+  { img: carYellow, x: randomXPlacement(), y: -1600, width: 130, height: 220 },
+  { img: bus, x: randomXPlacement(), y: -2300, width: 240, height: 520 },
+  { img: carPink, x: randomXPlacement(), y: -3000, width: 110, height: 170 },
+  {
+    img: carWhite,
+    x: randomXPlacement() - 300,
+    y: -3700,
+    width: 130,
+    height: 220,
+  },
+  { img: carYellow, x: randomXPlacement(), y: -4300, width: 130, height: 220 },
+  { img: bus, x: randomXPlacement(), y: -5000, width: 240, height: 520 },
+];
+
+//start game function
 function startGame() {
-    document.getElementById('game-board').style.display = 'block';
-    //restartBtn.style.display = 'block'
-    document.getElementById('start-button').style.display = 'none'
-    document.getElementById('logo-img').style.display = 'none'
-    document.getElementById('splash-text').style.display = 'none'
-    document.querySelector('.arrows-img').style.display = 'none'
-    //Instantiate a new game of the game class
-    currentGame = new Game();
-    //Instantiate a new DJ
-    currentDJ = new DJ();
-    currentGame.dj = currentDJ;
-    currentGame.dj.drawDJ();
-    updateCanvas();
-}
+  canvas.style.display = "block";
+  startBtn.style.display = "none";
+  logo.style.display = "none";
+  gameLogo.style.display = "none";
+  arrows.style.display = "none";
+  scoreDiv.style.display = "none";
+  gameBoard.style.display = "flex";
 
-function detectCollision(obstacle) {
-    return !((currentDJ.y > obstacle.y + obstacle.height) || 
-    (currentDJ.x + currentDJ.width < obstacle.x) || 
-    (currentDJ.x - currentDJ.width  > obstacle.x + obstacle.width))
-}
+  //draw background
+  ctx.drawImage(bg, 0, bg1Y, canvas.width, canvas.height);
+  //draw my second background
+  ctx.drawImage(bg, 0, bg2Y, canvas.width, canvas.height);
 
-let obstaclesFrequency = 0;
+  //moving background
+  //bg1Y += speed;
+  //bg2Y += speed;
+  //this checks for when the background reaches the bottom of canvas
+  // if (bg1Y > canvas.height) {
+  //   bg1Y = -canvas.height;
+  // }
+  // if (bg2Y > canvas.height) {
+  //   bg2Y = -canvas.height;
+  // }
 
-function updateCanvas() {
-    ctx.clearRect(0, 0, 500, 600);
-    const bgImg = new Image();
-    bgImg.src = '/images/crowd.jpeg'
-    ctx.drawImage(bgImg, 0, 0, myCanvas.width, myCanvas.height);
-    
-    currentGame.dj.drawDJ();
-    obstaclesFrequency++;
+  //draw DJ
+  ctx.drawImage(dj, playerX, playerY, playerWidth, playerHeight);
 
-    if (obstaclesFrequency % 100 === 1) {
-        //Draw an obstacle
-        let randomObstacleX = Math.floor(Math.random() * 450);
-        let randomObstacleY = 0;
-        let randomObstacleWidth = Math.floor(Math.random() * 50) + 20;
-        let randomObstacleHeight = Math.floor(Math.random() * 50) + 20;
-        let newObstacle = new Obstacle(
-            randomObstacleX, 
-            randomObstacleY, 
-            randomObstacleWidth, 
-            randomObstacleHeight);
-
-        currentGame.obstacles.push(newObstacle);
-        //console.log(currentGame.obstacles);
+  for (let i = 0; i < throwArray.length; i++) {
+    ctx.drawImage(
+      throwArray[i].img,
+      throwArray[i].x,
+      throwArray[i].y,
+      throwArray[i].width,
+      throwArray[i].height
+    );
+    throwArray[i].y += speed;
+    //ctx.drawImage(dj, middle + 50, height, 80, 150);
+    if (throwArray[i].y > canvas.height) {
+      throwArray[i].y = -5500;
     }
 
-    for(let i = 0; i<currentGame.obstacles.length; i++) {
-        currentGame.obstacles[i].x += 1;
-        currentGame.obstacles[i].drawObstacle();
-
-        if (detectCollision(currentGame.obstacles[i])) {
-            alert('OH NO COLLISION!')
-            obstaclesFrequency = 0;
-            currentGame.score = 0;
-            document.getElementById('score').innerHTML = 0;
-            currentGame.obstacles = [];
-            document.getElementById('game-board').style.display = 'none';
-        }
-
-        // Obstacle moved outside the canvas
-        if (currentGame.obstacles.length > 0 && currentGame.obstacles[i].y >= 600) {
-            currentGame.obstacles.splice(i, 1);
-            currentGame.score++;
-            document.getElementById('score').innerHTML = currentGame.score;
-        }
+    //score handling login (inside of for loop!), if traffic car passes player car... score ++
+    if (
+      throwArray[i].y > playerY + playerHeight &&
+      throwArray[i].y <= playerY + playerHeight + speed
+    ) {
+      score = score + 1;
+      scoreElement.innerHTML = score;
     }
 
-    requestAnimationFrame(updateCanvas);
+    //collision inside of for loop
+    if (
+      // checks if the bottom of the traffic car is touching the top of the player car
+      throwArray[i].y + throwArray[i].height >= playerY + 10 &&
+      //checks if the right side of the player car is more to the right than the traffic car
+      playerX + 120 > throwArray[i].x &&
+      // checks if the left side of the player car is touching the left side of the traffic car
+      playerX < throwArray[i].x + throwArray[i].width &&
+      //checks if the bottom of the player car is touching the top of the traffic car
+      playerY + playerHeight - 10 > throwArray[i].y
+    ) {
+      isGameOver = true;
+    }
+  }
+
+  if (playerX < canvas.width - 150 && moveRight) {
+    playerX += 5;
+  } else if (playerX > 35 && moveLeft) {
+    playerX -= 5;    
+  } else if (playerY > 35 && moveUp) {
+    playerY -= 5;
+  } else if (playerY > 35 && moveDown) {
+    playerY += 5;
+  }
+
+  //listener for player movement
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "ArrowRight") {
+      moveRight = true;
+    } else if (event.code === "ArrowLeft") {
+      moveLeft = true;
+    }
+      else if (event.code === "ArrowDown") {
+        moveDown = true;
+    }else if (event.code === "ArrowUp") {
+      moveUp = true;
+    }
+  });
+  document.addEventListener("keyup", () => {
+    moveRight = false;
+    moveLeft = false;
+    moveDown = false;
+    moveUp = false;
+  });
+
+  //if statement for game over
+  if (isGameOver) {
+    cancelAnimationFrame(intervalId);
+    gameover();
+  } else {
+    intervalId = requestAnimationFrame(startGame);
+  }
 }
+
+//restart game function
+function restartGame() {
+  canvas.style.display = "block";
+  startBtn.style.display = "none";
+  logo.style.display = "none";
+  arrows.style.display = "block";
+}
+
+function gameover() {
+  canvas.style.display = "none";
+  startBtn.style.display = "none";
+  logo.style.display = "none";
+  arrows.style.display = "none";
+  gameLogo.style.display = "block";
+  scoreDiv.style.display = "block";
+}
+
+  //game begins here
+window.addEventListener("load", () => {
+  canvas.style.display = "none";
+  startBtn.style.display = "block";
+  restartBtn.style.display = "none";
+  logo.style.display = "block";
+  gameLogo.style.display = "none";
+  arrows.style.display = "flex";
+  scoreDiv.style.display = "none";
+  gameBoard.style.display = "none";
+
+  startBtn.addEventListener("click", () => {
+    startGame();
+    console.log("start button pushed!");
+  });
+  restartBtn.addEventListener("click", () => {
+    restartGame();
+  });
+});
